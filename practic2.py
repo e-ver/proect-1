e5ac765a57d14948f8c9ac560e9f14223dbe9197
Eduard Vervikishko <eddoo@yandex.ru>
2023-01-16 08:19:30 +0000

    newpro

---
+++ practic2.py
@@ -0,0 +1,19 @@
+def spce():
+    print('\n__________________________________________')
+def counts(x):
+    count = 0
+    while x > 0:
+        x //= 10
+        count += 1
+    return count
+spce()
+x = 233
+print(counts(x))
+<<<<<<< HEAD
+print('XOXOXO', x)
+print('__________________________________________')
+=======
+print('XOXOXO',x)
+
+
+>>>>>>> 8588dbada495f778a2fe7faedc9f57a821f7c368
